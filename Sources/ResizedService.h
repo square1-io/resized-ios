d73bc0b4bcb00616dc71895e7de769d5f5e3e2c4
Rober Pastor <rober@square1.io>
2016-04-15 10:09:06 +0200

    New initializer to provide a general placeholder image URL. New method to provide a specific placeholder image URL just for a resizing.

--- Sources/ResizedService.h
+++ Sources/ResizedService.h
@@ -11,9 +11,26 @@
 @interface ResizedService : NSObject
 
 
-+ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url key:(NSString *)key;
-- (instancetype)initWithServerBaseURL:(NSString *)url key:(NSString *)key;
++ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url
+                                            key:(NSString *)key;
 
-- (NSString *)uriForImage:(NSString *)imageURL withSize:(CGSize)size;
++ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url
+                                            key:(NSString *)key
+                                    placeholder:(NSString *)placeholderURL;
+
+- (instancetype)initWithServerBaseURL:(NSString *)url
+                                  key:(NSString *)key;
+
+- (instancetype)initWithServerBaseURL:(NSString *)url
+                                  key:(NSString *)key
+                          placeholder:(NSString *)placeholderURL;
+
+- (NSString *)uriForImage:(NSString *)imageURL
+                 withSize:(CGSize)size;
+
+- (NSString *)uriForImage:(NSString *)imageURL
+                 withSize:(CGSize)size
+              placeholder:(NSString *)placeholderURL;
+;
 
 @end
--- Sources/ResizedService.m
+++ Sources/ResizedService.m
@@ -13,34 +13,67 @@
 
 @property NSString *serverBaseURL;
 @property NSString *key;
+@property NSString *placeholderURL;
 
 @end
 
 @implementation ResizedService
 
-+ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url key:(NSString *)key
++ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url
+                                            key:(NSString *)key
 {
-    return [[ResizedService alloc] initWithServerBaseURL:url key:key];
+    return [ResizedService resizedServiceWithServerBaseURL:url
+                                                       key:key
+                                               placeholder:nil];
 }
 
-- (instancetype)initWithServerBaseURL:(NSString *)url key:(NSString *)key
++ (instancetype)resizedServiceWithServerBaseURL:(NSString *)url
+                                            key:(NSString *)key
+                                    placeholder:(NSString *)placeholderURL
+{
+    return [[ResizedService alloc] initWithServerBaseURL:url
+                                                     key:key
+                                             placeholder:placeholderURL];
+}
+
+- (instancetype)initWithServerBaseURL:(NSString *)url
+                                  key:(NSString *)key
+{
+    return [self initWithServerBaseURL:url
+                                   key:key
+                           placeholder:nil];
+}
+
+- (instancetype)initWithServerBaseURL:(NSString *)url
+                                  key:(NSString *)key
+                          placeholder:(NSString *)placeholderURL
 {
     if (self = [super init]) {
         _serverBaseURL = url;
         _key = key;
+        _placeholderURL = placeholderURL;
     }
     
     return self;
 }
 
-- (NSString*)uriForImage:(NSString*)imageUrl withSize:(CGSize)size
+- (NSString*)uriForImage:(NSString*)imageUrl
+                withSize:(CGSize)size
 {
+    return [self uriForImage:imageUrl withSize:size placeholder:nil];
+}
+
+- (NSString*)uriForImage:(NSString*)imageUrl
+                withSize:(CGSize)size
+             placeholder:(NSString *)placeholderURL
+{
+    
     if (_serverBaseURL == nil || _key == nil) {
-        return nil;
+        return [self placeholderIfSupplied:placeholderURL];
     }
     
     if([imageUrl length] == 0){
-        return nil;
+        return [self placeholderIfSupplied:placeholderURL];
     }
     
     if (CGSizeEqualToSize(size, CGSizeZero)) {
@@ -55,24 +88,42 @@
         return imageUrl;
     }
     
-    NSString* data = @"";
-    
     NSMutableDictionary* mediaRequest = [NSMutableDictionary new];
-    
     [mediaRequest setObject:imageUrl forKey:@"url"];
     [mediaRequest setObject: size.width > 0 ? [NSNumber numberWithInt:size.width] : [NSNull null] forKey:@"width"];
     [mediaRequest setObject: size.height > 0 ? [NSNumber numberWithInt:size.height] : [NSNull null] forKey:@"height"];
     
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:mediaRequest options:0 error:nil];
-    data = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:mediaRequest
+                                                       options:0
+                                                         error:nil];
+    NSString *data = [[NSString alloc] initWithData:jsonData
+                                           encoding:NSUTF8StringEncoding];
     
     NSString* hash = [data sha1WithKey:_key];
     
-    NSDictionary* encodedData = [NSDictionary dictionaryWithObjectsAndKeys:data,@"data",hash,@"hash", nil];
-    jsonData = [NSJSONSerialization dataWithJSONObject:encodedData options:0 error:nil];
-    data = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSDictionary* encodedData = @{@"data":data,
+                                  @"hash":hash};
+    
+    jsonData = [NSJSONSerialization dataWithJSONObject:encodedData
+                                               options:0
+                                                 error:nil];
+    data = [[NSString alloc] initWithData:jsonData
+                                 encoding:NSUTF8StringEncoding];
     
     return [_serverBaseURL stringByAppendingString:[data base64EncodedString]];
+}
+
+- (NSString *)placeholderIfSupplied:(NSString *)placeholder
+{
+    if (!placeholder.isEmpty) {
+        return placeholder;
+    }
+    
+    if (!_placeholderURL.isEmpty) {
+        return _placeholderURL;
+    }
+    
+    return nil;
 }
 
 @end
